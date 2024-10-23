459f5bb379046f0d4f487f5e9386a19d5e69ad5d
Leosang-lx <1245977986@qq.com>
2024-10-23 13:29:40 +0800

    Initialize framework code

--- framework/conf_client.py
+++ framework/conf_client.py
@@ -5,25 +5,17 @@ import time
 
 class ConferenceClient:
     def __init__(self,):
-        """
-        建立与服务器的主要连接：仅用于与服务器的通信（保持连接）
-        """
+        # sync client
         self.is_working = True
         self.server_addr = None  # server addr
         self.on_meeting = False  # status
         self.conns = None  # you may need to maintain multiple conns for a single conference
         self.support_data_types = []  # for some types of data
+        self.share_data = {}
 
         self.conference_info = None  # you may need to save and update some conference_info regularly
 
         self.recv_data = None  # you may need to save received streamd data from other clients in conference
-
-
-    def init_conf_conn(self,):
-        """
-        init conns when create or join a conference with necessary conference_info
-        """
-        pass
 
     def create_conference(self):
         """
@@ -51,8 +43,8 @@ class ConferenceClient:
 
     def keep_share(self, data_type, send_conn, capture_function, compress=None, fps_or_frequency=30):
         '''
-        keep sharing (capture and send) certain type of data from server or clients (P2P)
-        you can create different functions for sharing various types of data
+        running task: keep sharing (capture and send) certain type of data from server or clients (P2P)
+        you can create different functions for sharing various kinds of data
         '''
         pass
 
@@ -62,123 +54,33 @@ class ConferenceClient:
         '''
         pass
 
-    def keep_recv(self, recv_conn, decompress=None):
+    def keep_recv(self, recv_conn, data_type, decompress=None):
         '''
-        keep receiving certain type of data
+        running task: keep receiving certain type of data (save or output)
+        you can create other functions for receiving various kinds of data
         '''
 
-    def recv_camera(self, decompress=None):
-        camera_sock = self.stream_socks['camera']
-        try:
-            while self.is_working:
-                client_id, frame = recv_data(camera_sock)
-                if decompress:
-                    frame = decompress(frame)
-                if self.camera_frames is None:
-                    self.camera_frames = [None] * 10
-                self.camera_frames[client_id] = frame
-                self.camera_tag = time.perf_counter()
-        except Exception as e:
-            print('Exception for sock_camera:', e)
+    def output_data(self):
+        '''
+        running task: output received stream data
+        '''
 
-    def recv_audio(self):
-        audio_sock = self.stream_socks['audio']
-        try:
-            while self.is_working:
-                client_id, audio_chunk = recv_data(audio_sock)
-                print('Recv chunk', len(audio_chunk))
-                streamout.write(audio_chunk)
-        except Exception as e:
-            print('Exception for sock_audio:', e)
-
-    def recv_msg(self):
-        try:
-            while self.is_working:
-                msg = recv_data(self.conference_sock)
-                print(msg)
-                msg = msg.strip().lower()
-                if 'client' in msg and msg.endswith('join the conference'):
-                    new_client_id = int(msg.split()[2])
-                    self.clients_in_conference.append(new_client_id)
-        except Exception as e:
-            print('Exception for sock_audio:', e)
-
-    def display_frames(self):
-        """
-        根据收到的视频帧以及音频节进行输出
-        """
-        last_screen_tag = None
-        last_camera_tag = None
-        while self.is_working:
-            if self.screen_frame is None and self.camera_frames is None:
-                time.sleep(0.1)
-            else:
-                update_screen = False
-                update_camera = False
-                if self.screen_frame is not None:
-                    # screen_tag, screen_frame = recv_screen
-                    if last_screen_tag is None or self.screen_tag != last_screen_tag:
-                        last_screen_tag = self.screen_tag
-                        update_screen = True
-                if self.camera_frames is not None:
-                    # camera_tag, camera_frames = recv_camera
-                    if last_camera_tag is None or self.camera_tag != last_camera_tag:
-                        last_camera_tag = self.camera_tag
-                        update_camera = True
-                if update_screen or update_camera:
-                    if self.camera_frames:
-                        recv_cameras = [ci for ci in self.camera_frames if ci is not None]
-                    else:
-                        recv_cameras = None
-
-                    display_frame = overlay_camera_images(self.screen_frame, recv_cameras)
-                    cv2.imshow(f'Client{self.client_id}', np.array(display_frame))
-                    cv2.waitKey(100)
-
-    def start_meeting(self):
-        # self.sock_screen = socket.create_connection((SERVER_IP, self.screen_port))
-        # self.sock_camera = socket.create_connection((SERVER_IP, self.camera_port))
-        # self.sock_audio = socket.create_connection((SERVER_IP, self.audio_port))
-
-        share_screen_thread = threading.Thread(target=self.share,
-                                               args=('screen', self.stream_socks['screen'], capture_screen, compress_image, 10))
-        share_camera_thread = threading.Thread(target=self.share,
-                                               args=('camera', self.stream_socks['camera'], capture_camera, compress_image, 10))
-        share_audio_thread = threading.Thread(target=self.share, args=('audio', self.stream_socks['audio'], capture_voice, None, 45))
-
-        recv_screen_thread = threading.Thread(target=self.recv_screen, args=(decompress_image,))
-        recv_camera_thread = threading.Thread(target=self.recv_camera, args=(decompress_image,))
-        recv_audio_thread = threading.Thread(target=self.recv_audio, args=())
-        recv_msg_thread = threading.Thread(target=self.recv_msg, args=())
-
-        self.recv_threads = [recv_screen_thread, recv_camera_thread, recv_audio_thread, recv_msg_thread]
-        self.send_threads = [share_screen_thread, share_camera_thread, share_audio_thread]
-        for t in self.recv_threads:
-            t.start()
-
-        for t in self.send_threads:
-            t.start()
-
-        display_threads = threading.Thread(target=self.display_frames)
-        display_threads.start()
-        # self.display_frames()
-
-    def close_threads(self):
-        """
-        关闭所有线程以及socket
-        """
-        self.is_working = False
-        for t in [*self.send_threads, *self.recv_threads]:
-            t.join()
+    def start_conference(self):
+        '''
+        init conns when create or join a conference with necessary conference_info
+        and
+        start necessary running task for conference
+        '''
 
     def close_conference(self):
-        self.close_conf_conns()
-        self.close_threads()
-        self.clients_in_conference = []
+        '''
+        close all conns to servers or other clients and cancel the running tasks
+        pay attention to the exception handling
+        '''
 
     def start(self):
         """
-        根据命令行输入执行指定功能
+        execute functions based on the command line input
         """
         while True:
             if not self.on_meeting:
@@ -206,11 +108,11 @@ class ConferenceClient:
                     if input_conf_id.isdigit():
                         self.join_conference(input_conf_id)
                     else:
-                        print('[Warn]: Input conference ID must be digital form')
+                        print('[Warn]: Input conference ID must be in digital form')
                 elif fields[0] == 'switch':
                     data_type = fields[1]
                     if data_type in self.share_data.keys():
-                        self.share_data[data_type] = not self.share_data[data_type]
+                        self.share_switch(data_type)
                 else:
                     recognized = False
             else:
@@ -221,6 +123,6 @@ class ConferenceClient:
 
 
 if __name__ == '__main__':
-    client1 = ConferenceClient(SERVER_IP, SERVER_MAIN_PORT)
+    client1 = ConferenceClient()
     client1.start()
 
--- framework/config.py
+++ framework/config.py
@@ -1,21 +1,16 @@
-"""
-字段全部用小写，方法全部用大写
-"""
-
 HELP = 'Create         : create an conference\n' \
        'Join [conf_id ]: join a conference with conference ID\n' \
        'Quit           : quit an on-going conference\n' \
        'Cancel         : cancel your on-going conference (only the manager)\n\n'
 
-# SERVER_IP = '127.0.0.1'
-SERVER_IP = '192.168.1.150'
-
-SERVER_MAIN_PORT = 8888
+SERVER_IP = '127.0.0.1'
+MAIN_SERVER_PORT = 8888
 TIMEOUT_SERVER = 5
-DGRAM_SIZE = 1500
+# DGRAM_SIZE = 1500  # UDP
+LOG_INTERVAL = 2
 
 CHUNK = 1024
-CHANNELS = 1  # 声道数
-RATE = 44100  # 采样率
+CHANNELS = 1  # Channels for audio capture
+RATE = 44100  # Sampling rate for audio capture
 
-camera_width, camera_height = 480, 480
+camera_width, camera_height = 480, 480  # resolution for camera capture
